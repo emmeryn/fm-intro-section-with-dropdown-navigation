7d801d6f23dcbf5cacfa5d0a048ff218fc890433
Sue Zheng Hao <szhlibrary@gmail.com>
2023-01-03 01:54:34 +0800

    NavBar: Add right-aligned Login/Register links

--- src/components/NavHeader/index.tsx
+++ src/components/NavHeader/index.tsx
@@ -12,17 +12,24 @@ export const NavHeader = () => {
     const logo = <div className={'logo'}>
         <a href={'#'}><Logo/></a>
     </div>;
-    const navItems: JSX.Element[] = navigation.map((item, idx) => {
-        return <div className={'navItem'}>
-            <a href={item.href}>{item.name}</a>
-        </div>
-    });
+    const navItems: JSX.Element = <div className={'navItems'}>
+        {navigation.map((item, idx) => {
+            return <div className={'navItem'}>
+                <a href={item.href}>{item.name}</a>
+            </div>
+        })}
+    </div>;
+    const userItems: JSX.Element = <div className={"userItems"}>
+        <a href={'#'}>Login</a>
+        <a href={'#'} className={'btn-outline'}>Register</a>
+    </div>
 
     return (
         <>
             <nav>
                 {logo}
                 {navItems}
+                {userItems}
             </nav>
         </>
     )
