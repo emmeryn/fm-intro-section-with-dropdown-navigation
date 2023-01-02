5d85ef05ec8d1d7a925eb1922a5c58e77619f01c
Sue Zheng Hao <szhlibrary@gmail.com>
2023-01-03 03:55:21 +0800

    Content: Implement content and hero image for desktop view

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import './App.css';
 import {NavHeader} from './components/NavHeader';
+import {Content} from './components/Content';
+import heroDesktop from './images/image-hero-desktop.png';
+import heroMobile from './images/image-hero-mobile.png';
 
 function App() {
   return (
@@ -8,6 +11,15 @@ function App() {
       <header className="App-header">
           <NavHeader />
       </header>
+      <main>
+        <Content/>
+        <div>
+          <picture>
+              <source media="(min-width: 60rem)" srcSet={heroDesktop}/>
+              <img src={heroMobile} alt="hero image"/>
+          </picture>
+        </div>
+      </main>
     </div>
   );
 }
---
+++ src/components/Content/index.tsx
@@ -0,0 +1,12 @@
+import './index.css';
+
+export const Content = () => {
+    return <div className={'content'}>
+        <h1>Make remote work</h1>
+        <p>
+            Get your team in sync, no matter your location. Streamline processes,
+            create team rituals, and watch productivity soar.
+        </p>
+        <a href={'#'} className={'btn-solid'}>Learn more</a>
+    </div>
+}
